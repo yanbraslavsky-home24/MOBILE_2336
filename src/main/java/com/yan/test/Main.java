75f9a1bd9a802c3ee36604f78d14a0c5bc5790d1
Yan Braslavsky <yan.braslavsky@home24.de>
2016-05-20 16:58:46 +0200

    Draft Working solution with compiled java classes into a JAR

--- src/main/java/com/yan/test/Main.java
+++ src/main/java/com/yan/test/Main.java
@@ -1,12 +1,17 @@
 package com.yan.test;
 
+import com.speaker.lib.Speaker6;
+
 /**
  * Created by yan.braslavski on 5/19/16.
  */
 public class Main {
 
     public static void main(String[] args) {
+
+        Speaker6 speaker = new Speaker6();
         System.out.println("Hello World!"); // Display the string.
+        speaker.speak("Here is alternate speaker speaking ! :)");
     }
 
 }
